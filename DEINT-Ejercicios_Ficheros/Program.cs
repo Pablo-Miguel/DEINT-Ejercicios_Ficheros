db09b426e162c5ddf1734ecff7c034739202a4e6
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-11-15 16:28:43 +0100

    V1

--- DEINT-Ejercicios_Ficheros/Program.cs
+++ DEINT-Ejercicios_Ficheros/Program.cs
@@ -2,136 +2,178 @@
 
 using System.Text.RegularExpressions;
 
-String ruta1;
-String ruta2;
+void ejercicio1() {
 
-//Ejercicio1
-/*
-Console.WriteLine("Introduzca ruta del archivo 1: ");
-ruta1 = Console.ReadLine();
-Console.WriteLine("Indroduzca ruta del archivo 2: ");
-ruta2 = Console.ReadLine();
+    //Ejercicio1
+    Console.WriteLine("EJERCICIO 1");
 
-File.WriteAllText(ruta2, File.ReadAllText(ruta1));
+    Console.WriteLine("Introduzca ruta del archivo 1: ");
+    String ruta1 = Console.ReadLine();
+    Console.WriteLine("Indroduzca ruta del archivo 2: ");
+    String ruta2 = Console.ReadLine();
 
-ruta1 = "";
-ruta2 = "";
+    File.WriteAllText(ruta2, File.ReadAllText(ruta1));
 
-Console.WriteLine("Se ha volcado el texto correctamente");
-*/
+    Console.WriteLine("Se ha volcado el texto correctamente");
+}
 
-//Ejercicio2
-/*
-Console.WriteLine("Introduzca ruta del archivo: ");
-ruta1 = Console.ReadLine();
+void ejercicio2() {
 
-Console.WriteLine("Indroduzca frase a buscar: ");
-String frase = Console.ReadLine();
+    //Ejercicio2
+    Console.WriteLine("EJERCICIO 2");
 
-String[] listaLineas = File.ReadAllLines(ruta1);
+    Console.WriteLine("Introduzca ruta del archivo: ");
+    String ruta1 = Console.ReadLine();
 
-for (int i = 0; i < listaLineas.Length; i++) {
-    if (listaLineas[i].Contains(frase))
+    Console.WriteLine("Indroduzca frase a buscar: ");
+    String frase = Console.ReadLine();
+
+    String[] listaLineas = File.ReadAllLines(ruta1);
+
+    for (int i = 0; i < listaLineas.Length; i++)
     {
-        if (Regex.IsMatch(frase, @"^.*" + frase + ".*$"))
+        if (listaLineas[i].Contains(frase))
         {
-            Console.WriteLine($"LINEA: {i + 1}, FRASE: {listaLineas[i]}");
+            if (Regex.IsMatch(frase, @"^.*" + frase + ".*$"))
+            {
+                Console.WriteLine($"LINEA: {i + 1}, FRASE: {listaLineas[i]}");
+            }
         }
-    } 
+    }
+
 }
 
-ruta1 = "";
-*/
+void ejercicio3() {
 
-//Ejercicio3
-/*
-Console.WriteLine("Introduzca ruta del archivo 1: ");
-ruta1 = Console.ReadLine();
-Console.WriteLine("Indroduzca ruta del archivo 2: ");
-ruta2 = Console.ReadLine();
+    //Ejercicio3
+    Console.WriteLine("EJERCICIO 3");
 
-File.Copy(ruta1, ruta2, true);
+    Console.WriteLine("Introduzca ruta del archivo 1: ");
+    String ruta1 = Console.ReadLine();
+    Console.WriteLine("Indroduzca ruta del archivo 2: ");
+    String ruta2 = Console.ReadLine();
 
-ruta1 = "";
-ruta2 = "";
+    File.Copy(ruta1, ruta2, true);
 
-Console.WriteLine("Se ha copiado el archivo correctamente");
-*/
+    Console.WriteLine("Se ha copiado el archivo correctamente");
+}
 
-//Ejercicio4
-/*
-Console.WriteLine("Introduzca ruta del archivo: ");
-ruta1 = Console.ReadLine();
+void ejercicio4() {
 
-String[] listaAlumnos = File.ReadAllLines(ruta1);
+    //Ejercicio4
+    Console.WriteLine("EJERCICIO 4");
 
-String mejorAlumno = "";
-int mejorNota = 0;
-int nota;
-int mediaMates = 0;
-foreach (var alumno in listaAlumnos)
-{
-    String[] listaNotas = alumno.Split(" ");
+    Console.WriteLine("Introduzca ruta del archivo: ");
+    String ruta1 = Console.ReadLine();
 
-    for (int i = 1; i < listaNotas.Length; i++)
+    String[] listaAlumnos = File.ReadAllLines(ruta1);
+
+    String mejorAlumno = "";
+    int mejorNota = 0;
+    int nota;
+    int mediaMates = 0;
+    foreach (var alumno in listaAlumnos)
     {
-        nota = Int16.Parse(listaNotas[i]);
+        String[] listaNotas = alumno.Split(" ");
 
-        if (i == 1) {
-            mediaMates += nota;
-        }
+        for (int i = 1; i < listaNotas.Length; i++)
+        {
+            nota = Int16.Parse(listaNotas[i]);
 
-        if (nota > mejorNota && i == 2) { 
-            mejorNota = nota;
-            mejorAlumno = listaNotas[0];
+            if (i == 1)
+            {
+                mediaMates += nota;
+            }
+
+            if (nota > mejorNota && i == 2)
+            {
+                mejorNota = nota;
+                mejorAlumno = listaNotas[0];
+            }
+
         }
 
     }
 
+    mediaMates = mediaMates / listaAlumnos.Length;
+
+    Console.WriteLine("Introduzca ruta del archivo resultado: ");
+    String ruta2 = Console.ReadLine();
+
+    File.WriteAllText(ruta2, $"MEDIA MATEMÁTICAS: {mediaMates}\nMEJOR ALUMNO DE INFORMÁTICA: " +
+        $"\n\t - Nombre: {mejorAlumno} \n\t - Nota: {mejorNota}");
+
 }
 
-mediaMates = mediaMates / listaAlumnos.Length;
-
-Console.WriteLine("Introduzca ruta del archivo resultado: ");
-ruta2 = Console.ReadLine();
-
-File.WriteAllText(ruta2, $"MEDIA MATEMÁTICAS: {mediaMates}\nMEJOR ALUMNO DE INFORMÁTICA: " +
-    $"\n\t - Nombre: {mejorAlumno} \n\t - Nota: {mejorNota}");
-
-ruta1 = "";
-ruta2 = "";
-*/
-
-//Ejercicio5
-/*
-Console.WriteLine("Introduzca ruta del archivo 1: ");
-ruta1 = Console.ReadLine();
-Console.WriteLine("Indroduzca ruta del archivo 2: ");
-ruta2 = Console.ReadLine();
-
-using (StreamWriter sw = new StreamWriter(Path.Combine(Path.GetDirectoryName(ruta1), "union.txt"), false))
+void ejercicio5()
 {
-    String[] lista1 = File.ReadAllLines(ruta1);
-    String[] lista2 = File.ReadAllLines(ruta2);
 
-    for (int i = 0, j = 0; i < lista1.Length || j < lista2.Length;) {
-        
-        if (i < lista1.Length) {
-            sw.WriteLine(lista1[i]);
-            i++;
+    //Ejercicio5
+    Console.WriteLine("EJERCICIO 5");
+
+    Console.WriteLine("Introduzca ruta del archivo 1: ");
+    String ruta1 = Console.ReadLine();
+    Console.WriteLine("Indroduzca ruta del archivo 2: ");
+    String ruta2 = Console.ReadLine();
+
+    using (StreamWriter sw = new StreamWriter(Path.Combine(Path.GetDirectoryName(ruta1), "union.txt"), false))
+    {
+        String[] lista1 = File.ReadAllLines(ruta1);
+        String[] lista2 = File.ReadAllLines(ruta2);
+
+        for (int i = 0, j = 0; i < lista1.Length || j < lista2.Length;)
+        {
+
+            if (i < lista1.Length)
+            {
+                sw.WriteLine(lista1[i]);
+                i++;
+            }
+            if (j < lista2.Length)
+            {
+                sw.WriteLine(lista2[j]);
+                j++;
+            }
+
         }
-        if (j < lista2.Length) {
-            sw.WriteLine(lista2[j]);
-            j++;
-        }
-    
+
     }
 
 }
 
-ruta1 = "";
-ruta2 = "";
-*/
+void ejercicio6() {
 
-//Ejercicio6 C:\Users\Dam\Desktop\enteros.txt
+    //Ejercicio6
+    Console.WriteLine("EJERCICIO 6");
 
+    String ruta1 = "C:\\Users\\Nitro\\Desktop\\enteros.txt";
+
+    String enteros = File.ReadAllText(ruta1);
+
+    MatchCollection numeros = Regex.Matches(enteros, @"\d+");
+
+    String cadNumeros = "";
+    int suma = 0;
+    foreach (Match num in numeros)
+    {
+
+        cadNumeros += num.Value + " ";
+        suma += Int32.Parse(num.Value);
+    }
+
+    Console.WriteLine($"Números: {cadNumeros}");
+    Console.WriteLine($"Cantidad de números: {numeros.Count}");
+    Console.WriteLine($"Suma: {suma}");
+}
+
+//ejercicio1();
+
+//ejercicio2();
+
+//ejercicio3();
+
+//ejercicio4();
+
+//ejercicio5();
+
+//ejercicio6();
